bc7c0498170272bde59bdaa9dd96c8f602e217d0
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2025-04-25 10:32:23 -0500

    fix:se ajustan endpoinst

--- payco/payco.php
+++ payco/payco.php
@@ -799,7 +799,7 @@ class Payco extends PaymentModule
                 $ref_payco = $_REQUEST["ref_payco"];
             }
 
-            $url = 'eks-checkout-service.epayco.io/validation/v1/reference/'.$ref_payco;
+            $url = 'https://eks-checkout-service.epayco.io/validation/v1/reference/'.$ref_payco;
         }
 
 
