5af09959da787b5cf6b087f1f68d1441a0894180
Ricardo Saldarriaga <40249987+RicardoSaldarriagaPayco@users.noreply.github.com>
2026-02-23 18:13:53 -0500

    Update chackout endpoint

--- payco/payco.php
+++ payco/payco.php
@@ -129,7 +129,7 @@ class Payco extends PaymentModule
      */
     public function hookDisplayHeader()
     {
-        $this->context->controller->registerJavascript('epayco-checkout', 'https://checkout.epayco.co/checkout-v2.js', ['position' => 'bottom', 'priority' => 150]);
+        $this->context->controller->registerJavascript('epayco-checkout', 'https://epayco-checkout-testing.s3.amazonaws.com/checkout.preprod-v2.js', ['position' => 'bottom', 'priority' => 150]);
         $this->context->controller->registerStylesheet(
             'epayco-checkout-css',
             $this->getPathUri() . 'views/css/back.css',
@@ -1458,3 +1458,4 @@ class Payco extends PaymentModule
         file_put_contents($logFile, "[$date] $message\n", FILE_APPEND);
     }
 }
+
